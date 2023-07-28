53caf037a780ba0aab0b61a54e3788aaeb656b27
kimoto22 <n22m305@matsu.shimane-u.ac.jp>
2023-07-28 17:09:00 +0900

    apiをたたけるようにした

--- gptodoproject/gptodo/views.py
+++ gptodoproject/gptodo/views.py
@@ -1,13 +1,29 @@
+import openai
 from django.shortcuts import render
-from .models import TodoItem
+from django.http import HttpResponse
+
+# ChatGPT APIキーを設定してください
+OPENAI_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXX" # 取得したAPIkeyをXXXと置き換える
+
+def generate_list(input_text):
+    openai.api_key = OPENAI_API_KEY
+
+    # ChatGPT APIを使ってリストを生成する
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=input_text,
+        temperature=0.7,
+        max_tokens=150
+    )
+
+    # APIのレスポンスから生成されたリストを取得する
+    generated_list = response.choices[0].text.strip()
+
+    return generated_list
 
 def index(request):
     if request.method == 'POST':
-        todo_input = request.POST['todo_input']
-        # ここでChatGPTを用いてTodoリストを生成する処理を実装する
-        # 生成されたTodoリストをtodo_listに格納する
-        todo_list = []
-
-        return render(request, 'gptodo/todo_list.html', {'todo_list': todo_list})
-    else:
-        return render(request, 'gptodo/index.html')
+        todo_input = request.POST.get('todo_input', '')
+        todo_list = generate_list(todo_input)
+        return render(request, 'gptodo/index.html', {'todo_input': todo_input, 'todo_list': todo_list})
+    return render(request, 'gptodo/index.html')
