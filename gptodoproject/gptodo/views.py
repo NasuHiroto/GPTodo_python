fead6397644308ef5045504ebd1f0dfa45af7759
kimoto22 <n22m305@matsu.shimane-u.ac.jp>
2023-07-28 16:31:51 +0900

    初期設定

--- gptodoproject/gptodo/views.py
+++ gptodoproject/gptodo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import TodoItem
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        todo_input = request.POST['todo_input']
+        # ここでChatGPTを用いてTodoリストを生成する処理を実装する
+        # 生成されたTodoリストをtodo_listに格納する
+        todo_list = []
+
+        return render(request, 'gptodo/todo_list.html', {'todo_list': todo_list})
+    else:
+        return render(request, 'gptodo/index.html')
